467cdf727eeaba0beffedb7e3ed9d874f1a01226
Guyguy25 <tanguybois5c@gmail.com>
2023-02-27 21:07:40 +0100

    add component dark/light mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,13 @@ document.querySelector("#nav-icon").addEventListener("click", function() {
 	document.querySelector("#nav-icon").classList.toggle("open");
 	document.querySelector(".overlay").classList.toggle("open");
 });
+
+$(".button-theme").click(function (){
+	if(document.querySelector(".button-theme.light") == document.querySelector(".button-theme.light")){
+		document.querySelector("html").classList.value = "dark"
+		document.querySelector(".button-theme").classList.value = "button-theme dark"
+	} else {
+		document.querySelector("html").classList.value = "light"
+		document.querySelector(".button-theme").classList.value = "button-theme light"
+	}
+})
