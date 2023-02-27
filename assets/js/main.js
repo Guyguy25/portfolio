b66b7e3cf38843f4827df231e08e1ac0af18b19a
Guyguy25 <tanguybois5c@gmail.com>
2023-02-27 23:14:22 +0100

    Light/Dark theme local storage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,22 @@
-document.querySelector("#nav-icon").addEventListener("click", function() {
-	document.querySelector("#nav-icon").classList.toggle("open");
-	document.querySelector(".overlay").classList.toggle("open");
+document.querySelector("#nav-icon").addEventListener("click", function () {
+  document.querySelector("#nav-icon").classList.toggle("open");
+  document.querySelector(".overlay").classList.toggle("open");
 });
 
-$(".button-theme").click(function (){
-	if(document.querySelector(".button-theme.light") == document.querySelector(".button-theme.light")){
-		document.querySelector("html").classList.value = "dark"
-		document.querySelector(".button-theme").classList.value = "button-theme dark"
-	} else {
-		document.querySelector("html").classList.value = "light"
-		document.querySelector(".button-theme").classList.value = "button-theme light"
-	}
-})
+html = document.querySelector("html");
+
+$(".button-theme").click(function () {
+  console.log("click");
+  if (html.classList.contains("dark")) {
+    html.classList.remove("dark");
+    html.classList.add("light");
+    localStorage.setItem("theme", "light");
+  } else {
+    html.classList.remove("light");
+    html.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+  }
+  document.getElementById("currentTheme").innerHTML = localStorage.theme;
+});
+
+html.classList.add(localStorage.getItem("theme"));
