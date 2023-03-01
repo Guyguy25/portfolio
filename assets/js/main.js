e21708a14200eacf16a41c408aa6b95b25b12f87
Guyguy25 <tanguybois5c@gmail.com>
2023-03-01 16:05:13 +0100

    styling language li + opti navbar in javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,49 @@
-/* Light/dark theme */
+// Removing class open when the window is larger than 800px width
+
+async function RemoveOpen() {
+  if (document.querySelector("#nav-icon").classList.contains("open")) {
+    if (window.innerWidth >= 800) {
+      document.querySelector("#nav-icon").classList.remove("open");
+      document.querySelector(".overlay").classList.remove("open");
+    }
+  }
+  if (document.querySelector(".languages").classList.contains("open")) {
+    if (window.innerWidth >= 800) {
+      document.querySelector(".languages").classList.remove("open");
+    }
+  }
+}
+
+// Check if it is under 800px width or not with an interval of 0.001 second
+setInterval(RemoveOpen, 1);
+
+// Removing class open when the window is larger than 800px width
+
+async function toggleRedirect() {
+  if (window.innerWidth <= 800) {
+    document
+      .querySelectorAll("nav > ul li")[0]
+      .setAttribute("onclick", "location.href=`/portfolio`");
+    document
+      .querySelectorAll("nav > ul li")[1]
+      .setAttribute("onclick", "location.href=`/portfolio/support`");
+    document
+      .querySelectorAll("nav > ul li")[3]
+      .setAttribute("onclick", "location.href=`https://github.com/guyguy25`");
+  }
+  if (window.innerWidth >= 800) {
+    if (document.querySelector("nav > ul > li").hasAttribute("onclick")) {
+      document.querySelectorAll("nav > ul li")[0].removeAttribute("onclick");
+      document.querySelectorAll("nav > ul li")[1].removeAttribute("onclick");
+      document.querySelectorAll("nav > ul li")[3].removeAttribute("onclick");
+    }
+  }
+}
+
+// Check if it is under 800px width or not with an interval of 0.001 second
+setInterval(toggleRedirect, 1);
+
+// Light/dark theme
 html = document.querySelector("html");
 
 $(".button-theme").click(function () {
@@ -13,66 +58,67 @@ $(".button-theme").click(function () {
   }
 });
 
-
 html.classList.add(localStorage.getItem("theme"));
 
-if(!localStorage.getItem("theme")) {
+if (!localStorage.getItem("theme")) {
   html.classList.value = "light";
 }
 
-/* Language traductor système */
+// Language traductor système
 
 langues = document.querySelector(".langues");
 english = document.querySelector(".en");
 french = document.querySelector(".fr");
 
 document.querySelector(".languages").addEventListener("click", function () {
-  document.querySelector(".languages").classList.toggle("open")
-})
+  if (window.innerWidth <= 800) {
+    document.querySelector(".languages").classList.toggle("open");
+  }
+});
 
-french.addEventListener("click", function() {
-  if(langues.classList.contains("en")) {
+french.addEventListener("click", function () {
+  if (langues.classList.contains("en")) {
     langues.classList.remove("en");
     langues.classList.add("fr");
-    html.setAttribute('lang', "fr");
+    html.setAttribute("lang", "fr");
     localStorage.setItem("languages", "fr");
-    location.reload()
+    location.reload();
   }
-})
+});
 
-english.addEventListener("click", function() {
-  if(langues.classList.contains("fr")) {
+english.addEventListener("click", function () {
+  if (langues.classList.contains("fr")) {
     langues.classList.remove("fr");
     langues.classList.add("en");
-    html.setAttribute('lang', "en");
+    html.setAttribute("lang", "en");
     localStorage.setItem("languages", "en");
-    location.reload()
+    location.reload();
   }
-})
+});
 
-html.setAttribute("lang", `${localStorage.getItem("languages")}`)
-langues.classList.add(localStorage.getItem("languages"))
+html.setAttribute("lang", `${localStorage.getItem("languages")}`);
+langues.classList.add(localStorage.getItem("languages"));
 
-if(!localStorage.getItem("languages")) {
-  html.setAttribute("lang", "en")
-  langues.classList.value = "langues en"
-  
-}else {
-  if(localStorage.getItem("languages") == "fr") {
-
-    document.querySelector(".test").innerHTML = "Salut, je suis un developpeur Français jeune et passionné. Je suis principalement spécialisé dans le developpement web mais je touche à tout."
-    document.querySelectorAll("nav > ul > li > a")[0].innerHTML = "Accueil"
-    document.querySelectorAll("nav > ul > li > a")[1].innerHTML = "Soutenir"
-    document.querySelector("nav > ul > li > span").innerHTML = "Langue"
+if (!localStorage.getItem("languages")) {
+  html.setAttribute("lang", "en");
+  langues.classList.value = "langues en";
+} else {
+  if (localStorage.getItem("languages") == "fr") {
+    document.querySelector(".test").innerHTML =
+      "Salut, je suis un developpeur Français jeune et passionné. Je suis principalement spécialisé dans le developpement web mais je touche à tout.";
+    document.querySelectorAll("nav > ul > li > a")[0].innerHTML = "Accueil";
+    document.querySelectorAll("nav > ul > li > a")[1].innerHTML = "Soutenir";
+    document.querySelector("nav > ul > li > span").innerHTML = "Langue";
   } else {
-    document.querySelector(".test").innerHTML = "Hi, I'm French developer full stack"
-    document.querySelectorAll("nav > ul > li > a")[0].innerHTML = "Home"
-    document.querySelectorAll("nav > ul > li > a")[1].innerHTML = "Support"
-    document.querySelector("nav > ul > li > span").innerHTML = "Language"
+    document.querySelector(".test").innerHTML =
+      "Hi, I'm French developer full stack";
+    document.querySelectorAll("nav > ul > li > a")[0].innerHTML = "Home";
+    document.querySelectorAll("nav > ul > li > a")[1].innerHTML = "Support";
+    document.querySelector("nav > ul > li > span").innerHTML = "Language";
   }
 }
 
-/* Nav button */
+// Nav button add open class
 
 document.querySelector("#nav-icon").addEventListener("click", function () {
   document.querySelector("#nav-icon").classList.toggle("open");
