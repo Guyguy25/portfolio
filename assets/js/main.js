181f40f31fc08b5ccaeb78dd343c6d16f7854963
Guyguy25 <tanguybois5c@gmail.com>
2023-03-06 18:59:49 +0100

    adding skills on nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,19 @@
+// ScrollReveal().reveal('.headline');
+// ScrollReveal().reveal('.tagline', { delay: 500 });
+// ScrollReveal().reveal('.punchline', { delay: 2000 });
+
 home_li = document.querySelectorAll("nav > ul li")[0];
 home_a = document.querySelectorAll("nav > ul li a")[0];
+
 about_li = document.querySelectorAll("nav > ul li")[1];
 about_a = document.querySelectorAll("nav > ul li a")[1];
-contact_li = document.querySelectorAll("nav > ul li")[2];
-contact_a = document.querySelectorAll("nav > ul li a")[2];
+
+skills_li = document.querySelectorAll("nav > ul li")[2];
+skills_a = document.querySelectorAll("nav > ul li a")[2];
+
+contact_li = document.querySelectorAll("nav > ul li")[3];
+contact_a = document.querySelectorAll("nav > ul li a")[3];
+
 main = document.querySelector("main");
 
 // Scroll Système
@@ -15,7 +25,7 @@ if(main.scrollTop == 0) {
 }
 
 main.addEventListener("scroll", () => {
-  if(main.scrollTop <= 900){
+  if(main.scrollTop < 1250){
     if(!home_a.classList.contains("active")) {
       home_a.classList.add("active")
     }
@@ -25,8 +35,11 @@ main.addEventListener("scroll", () => {
     if(contact_a.classList.contains("active")) {
       contact_a.classList.remove("active")
     }
+    if(skills_a.classList.contains("active")) {
+      skills_a.classList.remove("active")
+    }
   }
-  if(main.scrollTop >= 1000){
+  if(main.scrollTop >= 1250){
     if(!about_a.classList.contains("active")) {
       about_a.classList.add("active")
     }
@@ -36,8 +49,25 @@ main.addEventListener("scroll", () => {
     if(contact_a.classList.contains("active")) {
       contact_a.classList.remove("active")
     }
+    if(skills_a.classList.contains("active")) {
+      skills_a.classList.remove("active")
+    }
   }
-  if(main.scrollTop >= 1700){
+  if(main.scrollTop >= 2400){
+    if(!skills_a.classList.contains("active")) {
+      skills_a.classList.add("active")
+    }
+    if(home_a.classList.contains("active")) {
+      home_a.classList.remove("active")
+    }
+    if(about_a.classList.contains("active")) {
+      about_a.classList.remove("active")
+    }
+    if(contact_a.classList.contains("active")) {
+      contact_a.classList.remove("active")
+    }
+  }
+  if(main.scrollTop >= 3500){
     if(!contact_a.classList.contains("active")) {
       contact_a.classList.add("active")
     }
@@ -47,7 +77,11 @@ main.addEventListener("scroll", () => {
     if(about_a.classList.contains("active")) {
       about_a.classList.remove("active")
     }
+    if(skills_a.classList.contains("active")) {
+      skills_a.classList.remove("active")
+    }
   }
+  // console.log(main.scrollTop)
 })
 
 home_li.addEventListener('click', function () {
@@ -55,54 +89,74 @@ home_li.addEventListener('click', function () {
     top: 0,
     behavior: 'smooth',
   });
-  if(!home_a.classList.contains("active")) {
-    home_a.classList.add("active");
-  };
-  if(about_a.classList.contains("active")) {
-    about_a.classList.remove("active");
-  };
-  if(contact_a.classList.contains("active")) {
-    contact_a.classList.remove("active");
-  };
+  // if(!home_a.classList.contains("active")) {
+  //   home_a.classList.add("active");
+  // };
+  // if(about_a.classList.contains("active")) {
+  //   about_a.classList.remove("active");
+  // };
+  // if(contact_a.classList.contains("active")) {
+  //   contact_a.classList.remove("active");
+  // };
 })
 
 about_li.addEventListener('click', function () {
   main.scrollTo({
-    top: 1000,
+    top: 1250,
     behavior: 'smooth',
   });
-  if(!about_a.classList.contains("active")) {
-    about_a.classList.add("active");
-  };
-  if(home_a.classList.contains("active")) {
-    home_a.classList.remove("active");
-  };
-  if(contact_a.classList.contains("active")) {
-    contact_a.classList.remove("active");
-  };
+  // if(!about_a.classList.contains("active")) {
+  //   about_a.classList.add("active");
+  // };
+  // if(home_a.classList.contains("active")) {
+  //   home_a.classList.remove("active");
+  // };
+  // if(contact_a.classList.contains("active")) {
+  //   contact_a.classList.remove("active");
+  // };
+})
+
+skills_li.addEventListener('click', function () {
+  main.scrollTo({
+    top: 2400,
+    behavior: 'smooth',
+  });
+  // if(!about_a.classList.contains("active")) {
+  //   about_a.classList.add("active");
+  // };
+  // if(home_a.classList.contains("active")) {
+  //   home_a.classList.remove("active");
+  // };
+  // if(contact_a.classList.contains("active")) {
+  //   contact_a.classList.remove("active");
+  // };
 })
 
 contact_li.addEventListener('click', function () {
+  // if(!contact_a.classList.contains("active")) {
+  //   contact_a.classList.add("active");
+  // };
+  // if(home_a.classList.contains("active")) {
+  //   home_a.classList.remove("active");
+  // };
+  // if(about_a.classList.contains("active")) {
+  //   about_a.classList.remove("active");
+  // };
+  // if(skills_a.classList.contains("active")) {
+  //   skills_a.classList.remove("active")
+  // }
   main.scrollTo({
-    top: 1700,
+    top: 2400,
     behavior: 'smooth',
   });
-  if(!contact_a.classList.contains("active")) {
-    contact_a.classList.add("active");
-  };
-  if(home_a.classList.contains("active")) {
-    home_a.classList.remove("active");
-  };
-  if(about_a.classList.contains("active")) {
-    about_a.classList.remove("active");
-  };
+  
 })
 
-// Removing class open when the window is larger than 870px width
+// Removing class open when the window is larger than 970px width
 
 function RemoveOpen() {
   if (document.querySelector("#nav-icon").classList.contains("open")) {
-    if (window.innerWidth >= 870) {
+    if (window.innerWidth >= 970) {
       document.querySelector("#nav-icon").classList.remove("open");
       document.querySelector("nav").classList.remove("open");
       document.querySelector(".overlay").classList.remove("open");
@@ -114,13 +168,13 @@ function RemoveOpen() {
   }
 
   if (document.querySelector(".languages").classList.contains("open")) {
-    if (window.innerWidth >= 870) {
+    if (window.innerWidth >= 970) {
       document.querySelector(".languages").classList.remove("open");
     }
   }
 }
 
-// Check if it is under 870px width or not with an interval of 0.001 second
+// Check if it is under 970px width or not with an interval of 0.001 second
 setInterval(RemoveOpen, 1);
 
 
@@ -153,7 +207,7 @@ english = document.querySelector(".en");
 french = document.querySelector(".fr");
 
 document.querySelector(".languages").addEventListener("click", function () {
-  if (window.innerWidth <= 870) {
+  if (window.innerWidth <= 970) {
     document.querySelector(".languages").classList.toggle("open");
   }
 });
@@ -188,16 +242,18 @@ if (!localStorage.getItem("languages")) {
   if (localStorage.getItem("languages") == "fr") {
     document.querySelector(".description").innerHTML =
       "Salut, je suis un developpeur Français jeune et passionné. Je suis principalement spécialisé dans le developpement web mais je touche à tout.";
-    document.querySelectorAll("nav > ul > li > a")[0].innerHTML = "Accueil";
-    document.querySelectorAll("nav > ul > li > a")[1].innerHTML = "À propos";
-    document.querySelectorAll("nav > ul > li > a")[2].innerHTML = "Contacter";
+    home_a.innerHTML = "Accueil";
+    about_a.innerHTML = "À propos";
+    skills_a.innerHTML = "Compétences"
+    contact_a.innerHTML = "Contacter";
     document.querySelector("nav > ul > li > span").innerHTML = "Langue";
   } else {
     document.querySelector(".description").innerHTML =
       "Hi, I'm a young and passionate French developer. I'm mainly specialized in web development but I touch everything.";
-    document.querySelectorAll("nav > ul > li > a")[0].innerHTML = "Home";
-    document.querySelectorAll("nav > ul > li > a")[1].innerHTML = "About";
-    document.querySelectorAll("nav > ul > li > a")[2].innerHTML = "Contact";
+    home_a.innerHTML = "Home";
+    about_a.innerHTML = "About";
+    skills_a.innerHTML = "Skills"
+    contact_a.innerHTML = "Contact";
     document.querySelector("nav > ul > li > span").innerHTML = "Language";
   }
 }
